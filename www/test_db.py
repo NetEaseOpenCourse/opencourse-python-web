a556c1efbeb65c585780c1a927d25e99716d7656
lly <490859662@qq.com>
2017-08-29 12:01:30 +0800

    读取数据库模块

--- www/test_db.py
+++ www/test_db.py
@@ -7,8 +7,21 @@ from transwarp import db
 
 db.create_engine(user = 'root',password = 'lly880620',database = 'awesome')
 
-u = User(name = 'Test',email = 'test@163.com',password = '123456',image = 'about:blank')
+# u0 = User(name = 'Test',email = 'test@163.com',password = '123456',image = 'about:blank')
 
-u.insert()
+# u0.insert()
 
-print 'new user id',u.id
+u1 = User(name = 'Test001',email = 'test001@163.com',password = '123456',image = 'about:blank')
+
+u1.insert()
+
+u2 = User(name = 'Test002',email = 'test002@163.com',password = '123456',image = 'about:blank')
+
+u2.insert()
+
+u3 = User(name = 'Test003',email = 'test003@163.com',password = '123456',image = 'about:blank')
+
+u3.insert()
+
+
+# print 'new user id',u.id
